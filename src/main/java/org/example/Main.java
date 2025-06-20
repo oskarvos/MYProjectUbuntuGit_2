7b2d0049814799cade40cd2f9306e5e5e88932a7
oleg <os@mail.com>
2025-06-20 11:16:59 +0300

    commit branch main 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("commit branch to main");
+        System.out.printf("commit branch to main");
     }
 }
